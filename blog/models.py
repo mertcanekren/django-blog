4a239b346b3e74f443a7fccea0ac637642f4c48c
Mertcan Ekren <mertcanekren@windowslive.com>
2013-08-07 20:25:01 +0300

    create model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import validate_slug
 
-# Create your models here.
+class makale(models.Model):
+    baslik = models.CharField(max_length=60)
+    slug = models.SlugField(unique=True)
+    giris = models.TextField(max_length=500)
+    makale = models.TextField()
+    yayin_tarihi = models.DateTimeField("Yayinlanma Tarihi")
+     
+    def __unicode__(self):
+        return self.slug
